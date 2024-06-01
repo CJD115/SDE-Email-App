ff920577ee13025a7f417f93a0b1cd7ac422fe89
CJD115 <93295848+CJD115@users.noreply.github.com>
2024-06-01 20:17:53 +0100

    Add files via upload

---
+++ 2024-mar-sde-email-client-woolly-mammoths-main/2024-mar-sde-email-client-woolly-mammoths-main/src/components/Inbox/index.jsx
@@ -0,0 +1,11 @@
+import EmailList from "../EmailList/Index.jsx";
+
+const Inbox = () => {
+  return (
+    <div className="w-full">
+      <EmailList />
+    </div>
+  );
+};
+
+export default Inbox;
